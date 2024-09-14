38f3b842fba854c6eac0d6c518c638e845267dea
Hugo <hugomisery1@gmail.com>
2024-09-14 17:08:28 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,6 @@ export default function Home() {
         <Hero />
         <Grid />
         <RecentProjects />
-        <Clients />
         <Experience />
         <Approach />
         <Footer />
