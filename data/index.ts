08b159eb07fe3519785c5faae061871a314e4ccc
Hugo <hugomisery1@gmail.com>
2024-09-16 12:22:58 +0200

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -182,33 +182,34 @@ export const navItems = [
   export const workExperience = [
     {
       id: 1,
-      title: "Frontend Engineer Intern",
-      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+      title: "Data Analyst",
+      desc: "Analyse de données pour une plateforme immobilière, et pour des équipes marketing, immobilières et financières. Utilisation de Python et SQL.",
       className: "md:col-span-2",
       thumbnail: "/exp1.svg",
     },
     {
       id: 2,
-      title: "Mobile App Dev - JSM Tech",
-      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
-      className: "md:col-span-2", // change to md:col-span-2
+      title: "Développeur React",
+      desc: "Création d'applications web interactives et dynamiques en utilisant React, en mettant l'accent sur l'optimisation des performances et l'expérience utilisateur.",
+      className: "md:col-span-2",
       thumbnail: "/exp2.svg",
     },
     {
       id: 3,
-      title: "Freelance App Dev Project",
-      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
-      className: "md:col-span-2", // change to md:col-span-2
+      title: "Automation",
+      desc: "Automatisation des services clients, mise en place de formulaires, chatbots, et suivi des données pour améliorer l'efficacité et réduire les tâches redondantes.",
+      className: "md:col-span-2",
       thumbnail: "/exp3.svg",
     },
     {
       id: 4,
-      title: "Lead Frontend Developer",
-      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      title: "Conseil en Transformation Digitale",
+      desc: "Aide à la mise en place d'outils et de solutions digitales pour améliorer l'efficacité des services, et accompagner les équipes dans leur transformation numérique.",
       className: "md:col-span-2",
       thumbnail: "/exp4.svg",
     },
-  ];
+];
+
   
   export const socialMedia = [
     {
