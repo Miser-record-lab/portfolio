11e81a1ac8e2ebc29db52ef0616cd5cad73beaa3
Hugo <hugomisery1@gmail.com>
2024-09-16 12:29:33 +0200

    Update Approach.tsx

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -9,17 +9,15 @@ const Approach = () => {
   return (
     <section className="w-full py-20">
       <h1 className="heading">
-        My <span className="text-purple">approach</span>
+        Mon <span className="text-purple">fonctionnement</span>
       </h1>
       {/* remove bg-white dark:bg-black */}
       <div className="my-20 flex flex-col lg:flex-row items-center justify-center w-full gap-4">
         {/* add des prop */}
         <Card
-          title="Planning & Strategy"
+          title="Planification & Stratégie"
           icon={<AceternityIcon order="Phase 1" />}
-          des="We'll collaborate to map out your website's goals, target audience, 
-          and key functionalities. We'll discuss things like site structure, 
-          navigation, and content requirements."
+          des="Nous collaborons pour définir les besoins spécifiques à l'amélioration de l'un de vos services. Je vous propose ensuite des solutions et outils adaptés pour atteindre vos objectifs."
         >
           <CanvasRevealEffect
             animationSpeed={5.1}
@@ -28,11 +26,9 @@ const Approach = () => {
           />
         </Card>
         <Card
-          title="Development & Progress Update"
+          title="Développement & Mise à jour"
           icon={<AceternityIcon order="Phase 2" />}
-          des="Once we agree on the plan, I cue my lofi playlist and dive into
-          coding. From initial sketches to polished code, I keep you updated
-          every step of the way."
+          des="Une fois le plan validé, je mets ma playlist lofi et plonge dans le code. Des premiers croquis aux lignes de code raffinées, je vous tiens informé de chaque étape du processus."
         >
           <CanvasRevealEffect
             animationSpeed={3}
@@ -50,11 +46,9 @@ const Approach = () => {
           {/* <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" /> */}
         </Card>
         <Card
-          title="Development & Launch"
+          title="Développement & Implémentation"
           icon={<AceternityIcon order="Phase 3" />}
-          des="This is where the magic happens! Based on the approved design, 
-          I'll translate everything into functional code, building your website
-          from the ground up."
+          des="C'est là que la magie opère ! Sur la base du design approuvé, je traduis tout en code fonctionnel et construis votre solution en partant de zéro, en implémentant la stratégie avec soin."
         >
           <CanvasRevealEffect
             animationSpeed={3}
