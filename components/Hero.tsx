08a0e14a8ef5fe6559f8d585a704a61b9dcf5038
Hugo <hugomisery1@gmail.com>
2024-09-16 12:50:30 +0200

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -37,6 +37,16 @@ const Hero = () => {
                     />
                 </a>
 
+                <div className="flex items-center md:gap-3 gap-6 mt-5">
+                    {socialMedia.map((profile) => (
+                        <Link key={profile.id} href={profile.link} target="_blank">
+                          <div className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border-black-300">
+                            <img src={profile.img} alt="icons" width={20} height={20} />
+                          </div>
+                        </Link>
+                    ))}
+                </div>
+
             </div>
         </div>
     </div>
